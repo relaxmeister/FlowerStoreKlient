be77ba52fcc3d7e3a2aced1c52eb95739d87c1d0
David Sodergren <david.sodergren94@gmail.com>
2019-05-23 21:23:21 +0200

    fixat till register sidan

--- server.js
+++ server.js
@@ -12,18 +12,32 @@ $(document).ready(function() {
         var address = document.getElementById("address").value;
         var password = document.getElementById("password").value;
 
-        var data = {firstName: fname, lastName: lname,
-            email: email, address: address, password: password};
+        var data = {
+            firstName: fname, 
+            lastName: lname,
+            email: email,
+            address: address, 
+            password: password};
             console.log(data);
 
         $.ajax({
-            url: '/kopiera url',
+            url: 'http://localhost:8080/customer/createCustomer',
+            crossDomain: true,
             type: 'POST',
             data: JSON.stringify(data),
             contentType: "application/json; charset=utf-8",
             dataType: "json",
-            async: false  
-        })
+            async: true, 
+            success: function(responseData, textStatus, jqXHR) {
+                var value = responseData.someKey;
+            },
+            error: function (responseData, textStatus, errorThrown) {
+                alert('POST failed.');
+            }
+        });
+
+
+
     });
 
 })
