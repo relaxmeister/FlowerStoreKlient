50b34991187cc7c95923f8449632ffd0637e48f2
David Sodergren <david.sodergren94@gmail.com>
2019-05-24 14:02:13 +0200

    all backend done

--- admin.js
+++ admin.js
@@ -1,17 +1,26 @@
 const everyUser = [];
+const currentCustomer = 0;
+const x = 1;
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, {});
+  });
+
 
 
 function displayAllUsers(allUsers){
     allUsers.forEach(element => {
         everyUser.push(element);
-        document.getElementById("customerList").innerHTML += "<li onclick=showCustomerInfo(" + `${element.id}` + ")>" + `${element.firstName}` +"</li>" 
+        document.getElementById("customerList").innerHTML += "<tr data-target='modal1' class='modal-trigger' onclick=setCurrentCustomer(" + `${element.id}` + ")>" +"<td>" + `${element.firstName}` + "</td>" + "<td>" + `${element.lastName}` + "</td>" + "<td>" + `${element.email}` + "</td>" +"</tr>";
     });
+
+       
 }
 
 async function getAllUsers(){
 
      fetch("http://localhost:8080/customer/getAllCustomers", {
-          mode: 'cors',
           headers: {
               'Content-Type': 'application/json',
           },
@@ -22,14 +31,23 @@ async function getAllUsers(){
         })
 }
 
+function setCurrentCustomer(customerId){
+    const customer = findCustomerById(customerId);
+    console.log(customer);
+    document.getElementById("customer-modal-order").innerHTML = "Ordrar för " + customer.firstName + " " + customer.lastName + " ORDERS: "
+    + customer.orders.for;
+
+}
+
 function showCustomerInfo(customerInfo){
     const customer = findCustomerById(customerInfo);
-    alert(customer.id + " " + customer.firstName);
+    console.log(customer);
+    alert(customer.address + " " + customer.orders.length);
 }
 
 function findCustomerById(customerId){
     return everyUser.filter(element => element.id == customerId)[0];
 }
 
-getAllUsers();
 
+getAllUsers();
