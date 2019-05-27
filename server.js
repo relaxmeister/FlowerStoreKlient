dc5ce6a8d3b183f2aed0839df7247b538c35d9f3
David Sodergren <david.sodergren94@gmail.com>
2019-05-27 13:32:15 +0200

    last commit

--- server.js
+++ server.js
@@ -12,33 +12,45 @@ $(document).ready(function() {
         var address = document.getElementById("address").value;
         var password = document.getElementById("password").value;
 
+
+        var inputs = [...document.getElementsByTagName("input")];
+        var allFieldsOk = true;
+
+        inputs.forEach(element => {
+            if(element.value == ""){
+                allFieldsOk = false;
+            }
+        });
+
         var data = {
             firstName: fname, 
             lastName: lname,
             email: email,
             address: address, 
             password: password};
-            console.log(data);
 
-        $.ajax({
-            url: 'http://localhost:8080/customer/createCustomer',
-            crossDomain: true,
-            type: 'POST',
-            data: JSON.stringify(data),
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            async: true, 
-            success: function(responseData, textStatus, jqXHR) {
-                var value = responseData.someKey;
-                alert("Användare skapad, du skickas nu tillbaka till startsidan");
-                window.location.href = "http://127.0.0.1:5500/index.html";
-            },
-            error: function (responseData, textStatus, errorThrown) {
-                alert('Alla fält är inte korrekt ifyllda');
-            }
-        });
+        if(allFieldsOk == true){
 
-
+            $.ajax({
+                url: 'http://localhost:8080/customer/createCustomer',
+                crossDomain: true,
+                type: 'POST',
+                data: JSON.stringify(data),
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                async: true, 
+                success: function(responseData, textStatus, jqXHR) {
+                    var value = responseData.someKey;
+                    alert("Användare skapad, du skickas nu tillbaka till startsidan");
+                    window.location.href = "http://127.0.0.1:5500/index.html";
+                },
+                error: function (responseData, textStatus, errorThrown) {
+                    alert('Email redan registrerad, prova en annan!');
+                }
+            });
+        }else{
+            alert("Alla fält måste fyllas i!");
+        }
 
     });
 
